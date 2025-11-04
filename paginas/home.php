fed6dbfe503abb241da34d8bea546df60ae368f5
Ruben Costa <rubencosta.dev.2004@gmail.com>
2025-11-04 22:15:04 +0000

    Created Header on Home Page!

--- paginas/home.php
+++ paginas/home.php
@@ -7,6 +7,7 @@
 
     <!-- CSS Files -->
     <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="home.css">
 
     <!-- Font Link -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -14,6 +15,32 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1>Welcome to FelixUberShop!</h1>
+    <div class="navbar">
+        <div class="navbar-container">
+            <a href="./" class="navbar-left">
+                <div class="navbar-icon">
+                    <div class="navbar-icon-container img-container"></div>
+                </div>
+
+                <h1>FelixUber<br/>Shop</h1>
+            </a>
+
+            <div class="navbar-middle">
+                <a href="">DISCOVER</a>
+                <a href="">DISCOUNTS</a>
+                <a href="">FOOTER</a>
+            </div>
+
+            <div class="navbar-right">
+                <div class="profile hide">
+                    <h2>JD</h2>
+                </div>
+
+                <a href="" class="signin">
+                    <p>SignIn</p>
+                </a>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
