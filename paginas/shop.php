f31942c009834ec2d5f78bc260c4affedde708ea
Ruben Costa <rubencosta.dev.2004@gmail.com>
2025-11-10 11:19:30 +0000

    FUS-2 | Front-End | Create Shop Page!

--- paginas/shop.php
+++ paginas/shop.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FelixUberShop - Shop</title>
+    <title>FelixUberShop - Home</title>
 
     <!-- CSS Files -->
     <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="shop.css">
 
     <!-- Font Link -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -14,6 +15,102 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1>Welcome to the Shop!</h1>
+    <div class="navbar">
+        <div class="navbar-container">
+            <a href="./" class="navbar-left">
+                <div class="navbar-icon">
+                    <div class="navbar-icon-container img-container"></div>
+                </div>
+
+                <h1>FelixUber<br/>Shop</h1>
+            </a>
+
+            <div class="navbar-middle">
+                <a href="">DISCOVER</a>
+                <a href="">DISCOUNTS</a>
+                <a href="">FOOTER</a>
+            </div>
+
+            <div class="navbar-right">
+                <div class="profile hide">
+                    <h2>JD</h2>
+                </div>
+
+                <a href="" class="signin">
+                    <p>SignIn</p>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="categories">
+        <div class="categories-container">
+            <div class="category-box">
+                <div class="fruits img-container">
+                    <div class="category-overlay">
+                        <h2>Fruits</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="category-box">
+                <div class="vegetables img-container">
+                    <div class="category-overlay">
+                        <h2>Vegetables</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="category-box">
+                <div class="meats img-container">
+                    <div class="category-overlay">
+                        <h2>Meats</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="category-box">
+                <div class="cereals img-container">
+                    <div class="category-overlay">
+                        <h2>Cereals</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="category-box">
+                <div class="wealth img-container">
+                    <div class="category-overlay">
+                        <h2>Wealth</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="products">
+        <div class="products-container">
+            <div class="product">
+                <div class="product-container">
+                    <div class="product-banner">
+                        <div class="product-img img-container"></div>
+                    </div>
+
+                    <div class="product-info">
+                        <h2>Chicken</h2>
+                        <div class="product-price">
+                            <p>9.99 EUR</p>
+                            <p class="hide">1.99 EUR</p>
+                        </div>
+
+                        <button class="product-btn">Add to Cart</button>
+                    </div>
+                </div>
+
+                <div class="product-discount hide">
+                    <p><span>50</span>% OFF</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
