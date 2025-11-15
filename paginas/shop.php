d26c3544137257e95595b00fabc3592417c52671
Ruben Costa <rubencosta.dev.2004@gmail.com>
2025-11-15 14:40:18 +0000

    FUS-3 | Front-End | Create SignIn Page!

--- paginas/shop.php
+++ paginas/shop.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="logo-green.png" type="image/x-icon">
     <title>FelixUberShop - Home</title>
 
     <!-- CSS Files -->
